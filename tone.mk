aecc6796d2560b1a5092736cf16829d3c24dc886
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-14 16:26:14 +0200

    tone-common: Build vendor variant of android.hidl.base@1.0
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.tui_comm@1.0-service-qti": library "android.hidl.base@1.0.so" not found: needed by /vendor/lib64/vendor.qti.hardware.tui_comm@1.0_vendor.so in namespace (default)
    
    Change-Id: I4c01db7e8d37c8a033c6b0b5c504982de6a45ad3

--- tone.mk
+++ tone.mk
@@ -241,6 +241,8 @@ PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor \
+    android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
     libhidltransport \
     libhidltransport.vendor \
