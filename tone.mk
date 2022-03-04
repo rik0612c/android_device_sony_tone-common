5d1fd945b5e326e99d662d652df49ca2a657a947
Etienne Ruffieux <eruffieux@google.com>
2022-03-04 13:31:43 +0000

    tone-common: Remove audio a2dp from common.mk
    Bug: 220306859
    Test: atest
    Tag: #feature
    Change-Id: I1af8461f8ad82afbb21b43dd6968e824ea89f5d6

--- tone.mk
+++ tone.mk
@@ -95,7 +95,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.1-impl \
-    audio.a2dp.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
     audio.usb.default \
