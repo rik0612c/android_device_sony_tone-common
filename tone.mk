df85551821d8c164258b4cc32746b3be5112810e
rik0612c <qq872638399@gmail.com>
2023-03-14 14:31:11 +0800

    fixup! tone-common: Build vendor variant of android.hidl.base@1.0

--- tone.mk
+++ tone.mk
@@ -242,7 +242,6 @@ PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
-    android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
     libhidltransport \
     libhidltransport.vendor \
