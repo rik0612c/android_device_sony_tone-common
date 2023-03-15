f888e0505ddc9193089a1dcdaa13a0895c7ce2fe
rik0612c <qq872638399@gmail.com>
2023-03-15 09:52:24 +0800

    fixup! tone-common: Enable AOSP bluetooth audio HAL v2

--- tone.mk
+++ tone.mk
@@ -127,6 +127,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
     android.hardware.bluetooth@1.0-service \
